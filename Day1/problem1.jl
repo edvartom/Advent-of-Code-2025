5f4490253883ee73d5702145dc0b7eae22e68eea
edvartom <edvartom@sutd.ntnu.no>
2025-12-09 18:37:05 +0100

    Changed some variables in problem 1, havent checked that it still works!! Wrote code for problem 2, but the answer is too high!!

--- Day1/problem1.jl
+++ Day1/problem1.jl
@@ -1,24 +1,28 @@
 "Takes a rotation and a current position, and returns the new position."
-function new_position(old_position::Int, rotation::String)
+function new_position(old_position::Int,
+                      rotation::String,
+                      nr_of_positions::Int = 100,
+                      min_position::Int = 0,
+                      max_position::Int = 99)
     nr_of_steps::Int = parse(Int, rotation[2:end])
-    new_position::Int = 0
+    new_position::Int = min_position
     if rotation[1] == 'L'
         new_position = old_position - nr_of_steps
     else
         new_position = old_position + nr_of_steps
     end
-    while new_position < 0
-        new_position += 100
+    while new_position < min_position
+        new_position += nr_of_positions
     end
-    while new_position > 99
-        new_position -= 100
+    while new_position > max_position
+        new_position -= nr_of_positions
     end
     return new_position
 end
 
 "For each line in the txt-file, update the position. Count the zeros and find the password"
-function change_position(filepath::String)
-    current_point::Int = 50
+function change_position(filepath::String, starting_point::Int = 50)
+    current_point::Int = starting_point
     password::Int = 0
     for rotation::String in eachline(filepath)
         current_point = new_position(current_point, rotation)
---
+++ Day1/problem2.jl
@@ -0,0 +1,37 @@
+"Takes a rotation and a current position, and returns the new position."
+function new_position(old_position::Int,
+                      rotation::String,
+                      nr_of_positions::Int = 100,
+                      min_position::Int = 0,
+                      max_position::Int = 99)
+    nr_of_steps::Int = parse(Int, rotation[2:end])
+    new_position::Int = min_position
+    counter::Int = 0
+    if rotation[1] == 'L'
+        new_position = old_position - nr_of_steps
+    else
+        new_position = old_position + nr_of_steps
+    end
+    while new_position < min_position
+        new_position += nr_of_positions
+        counter += 1
+    end
+    while new_position > max_position
+        new_position -= nr_of_positions
+        counter += 1
+    end
+    return new_position, counter
+end
+
+"For each line in the txt-file, update the position. Count the zeros and find the password"
+function change_position(filepath::String, starting_point::Int = 50)
+    current_point::Int = starting_point
+    password::Int = 0
+    for rotation::String in eachline(filepath)
+        current_point, counter = new_position(current_point, rotation)
+        password += counter
+    end
+    return password
+end
+
+change_position("Day1/input1.txt")
